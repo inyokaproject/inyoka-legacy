c913533973a0ef1043ed4b4df4c1569cbf1d1d56
apollo13 <apollo13@apolloner.eu>
2009-08-24 20:56:29 +0200

    add middlewares, should start documenting now

--- inyoka/application.py
+++ inyoka/application.py
@@ -15,7 +15,7 @@ from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.routing import Map
 
 from inyoka import setup_components
-from inyoka.core.api import IController
+from inyoka.core.api import IController, IMiddleware
 
 class Request(RequestBase):
     pass
@@ -35,17 +35,28 @@ class InyokaApplication(object):
         """Make the application object a WSGI application."""
 
         request = Request(environ, self)
+        response = None
         urls = self.url_map.bind_to_environ(environ)
 
-        try:
-            try:
-                endpoint, args = urls.match()
-                response = IController.get_view(endpoint)(request, **args)
-            except NotFound, e:
-                response = self.handle_not_found(request, e)
+        for middleware in IMiddleware.middlewares():
+            response = middleware.process_request(request)
+            if response is not None:
+                break
 
-        except HTTPException, e:
-            response = e.get_response(request)
+        if response is None:
+            try:
+                try:
+                    endpoint, args = urls.match()
+                    response = IController.get_view(endpoint)(request, **args)
+                except NotFound, e:
+                    response = self.handle_not_found(request, e)
+
+            except HTTPException, e:
+                response = e.get_response(request)
+
+        for middleware in reversed(IMiddleware.middlewares()):
+            response = middleware.process_response(request, response)
+
 
         # TODO: add session cleanup
         return ClosingIterator(response(environ, start_response),
--- inyoka/core/api.py
+++ inyoka/core/api.py
@@ -1,2 +1,3 @@
 from inyoka.core.controller import IController
 from inyoka.core.auth import IPermissionChecker
+from inyoka.core.middlewares import IMiddleware

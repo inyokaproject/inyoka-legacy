b26785eefd5f15bc78847acf6908f31b32ece59c
maix <none@none>
2009-09-06 00:52:43 +0200

    added DateConverter

--- inyoka/core/routing.py
+++ inyoka/core/routing.py
@@ -6,14 +6,32 @@
     :copyright: 2009 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
+import sre_constants
+import re
+from datetime import datetime
 from werkzeug.routing import Submount, Subdomain, EndpointPrefix, \
-    Rule as BaseRule
+    Rule as BaseRule, BaseConverter, ValidationError
+from werkzeug import url_quote
 from inyoka import Component
 from inyoka.utils.urls import make_full_domain
 
 #XXX: temporary to check if the routing stuff works until ente has finished
 #     working on config ;-)
 #from inyoka.core.config import config
+
+_date_formatter_split_re = '(%.)'
+_date_formatter_mapping = {
+    'd': r'\d\d',
+    'j': r'\d{3}',
+    'm': r'\d\d',
+    'U': r'\d\d',
+    'w': r'\d',
+    'W': r'\d\d',
+    'y': r'\d\d',
+    'Y': r'\d{4}',
+    '%': r'%',
+}
+
 config = {
     'routing.portal.subdomain': '',
     'routing.portal.submount': '/',
@@ -93,3 +111,33 @@ class Rule(BaseRule):
     def __gt__(self, endpoint):
         self.endpoint = endpoint
         return self
+
+
+class DateConverter(BaseConverter):
+    def __init__(self, map, format='%Y/%m/%d'):
+        # convert format string to a regex so that we don't have to be greedy.
+        regex = []
+        for part in _date_formatter_split_re.split(format):
+            if part.startswith('%'):
+                if part[1] not in _date_formatter_mapping:
+                    raise ValidationError('formatter not allowed for dates: %s'
+                                          % part)
+                regex.append(_date_formatter_mapping[part[0]])
+            else:
+                regex.append(re.escape(part))
+        self.regex = ''.join(regex)
+        self.format = format
+
+    def to_python(self, value):
+        try:
+            return datetime.strptime(value, self.format).date()
+        except (ValueError, sre_constants.error):
+            raise ValidationError('strptime failed. Either you supplied an '
+                                  'invalid format string or invalid data.')
+
+    def to_url(self, value):
+        try:
+            return url_quote(value.strftime(self.format))
+        except sre_constants.error:
+            raise ValidationError('strftime failed. Format string seems to '
+                                  'be invalid.')

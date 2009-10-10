c4649f2a9cf9a81789fb330a81690ae0f2189a5e
apollo13 <apollo13@apolloner.eu>
2009-10-10 16:31:10 +0200

    fix component tests, we need to think about component subclasses, do we want that?

--- tests/core/test_components.py
+++ tests/core/test_components.py
@@ -21,18 +21,11 @@ class ComponentType(Component):
 class ComponentType2(Component):
     pass
 
-class Subcomponent(ComponentType):
-    pass
-
 class Implementation(ComponentType):
     pass
 
 class Implementation2(ComponentType, ComponentType2):
     pass
-
-class Implementation3(Subcomponent):
-    pass
-
 
 def test_components():
     assert_true(ComponentType._iscomptype)
@@ -40,20 +33,15 @@ def test_components():
 
     eq_(Implementation._comptypes, [ComponentType])
     eq_(Implementation2._comptypes, [ComponentType, ComponentType2])
-    #XXX: here we go with a second fancy thing... Why is `ComponentType`
-    #     twice in the list?
-    eq_(Implementation3._comptypes, [ComponentType, Subcomponent])
-
 
 def test_setup_components():
     map = setup_components(['tests.core.test_components.*'])
     print map, len(map)
-    eq_(len(map), 4)
+    eq_(len(map), 2)
     assert_true(isinstance(map[Implementation], Implementation))
     assert_true(isinstance(map[Implementation2], Implementation2))
-    assert_true(isinstance(map[Implementation3], Implementation3))
+    #assert_true(isinstance(map[Implementation3], Implementation3))
     assert_false(ComponentType in map)
-    eq_(len(ComponentType._implementations), 4)
-    eq_(len(ComponentType2._implementations), 1)
-    #XXX: why in gods name does this return `4` not `1`?
-    eq_(len(Subcomponent._implementations), 1)
+    assert_false(ComponentType2 in map)
+    eq_(len(ComponentType.get_component_classes()), 2)
+    eq_(len(ComponentType2.get_component_classes()), 1)

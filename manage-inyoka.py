0cc812b19a8311e676ae6194782407c96740188b
apollo13 <apollo13@apolloner.eu>
2009-11-02 17:50:58 +0100

    remove dbshell and fix action_runserver

--- manage-inyoka.py
+++ manage-inyoka.py
@@ -21,32 +21,6 @@ def make_shell():
     return locals()
 
 
-def action_dbshell():
-    import sys
-    from subprocess import Popen
-    from sqlalchemy.engine.url import make_url
-    from inyoka.core.config import config
-    url = make_url(config['database_uri'])
-
-    #TODO: add more drivers here and use special database options
-    cmd = []
-    if url.drivername == 'mysql':
-        cmd.extend(('mysql', url.database))
-        if url.username:
-            cmd.extend(('-u', url.username))
-        if url.password:
-            cmd.append('-p%s' % url.password)
-        if url.host:
-            cmd.extend(('-h', url.host))
-        if url.port:
-            cmd.extend(('-P', url.port))
-    elif url.drivername == 'sqlite':
-        cmd.extend(('sqlite3', url.database))
-
-    p = Popen(cmd, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
-    return p.wait()
-
-
 def action_runtests(cleanup=True, debug=True):
     """Run the unit and doctests"""
     import sys
@@ -74,11 +48,18 @@ def action_runtests(cleanup=True, debug=True):
     from inyoka.core.test import run_suite
     run_suite(tests_path, cleanup)
 
+def make_runserver():
+    # initialize the app
+    from inyoka.application import application
+    from inyoka.core.api import config
+    parts = config['base_domain_name'].split(':')
+    port = int(parts[1]) if len(parts) == 2 else 80
+    return script.make_runserver(make_app, port=port, use_reloader=True,
+                                 use_debugger=True, use_evalex=True)
 
 action_shell = script.make_shell(make_shell, 'Interactive Inyoka Shell')
 # TODO make configureable
-action_runserver = script.make_runserver(make_app,
-    use_reloader=True, use_debugger=True, use_evalex=True)
+action_runserver = make_runserver()
 
 
 def print_version():

567b18198c72cb1c687edd3c02407704ee84a0cf
EnTeQuAk <none@none>
2009-11-12 21:30:59 +0100

    import all exceptions in core.api

--- inyoka/core/api.py
+++ inyoka/core/api.py
@@ -11,6 +11,6 @@ from inyoka.core.routing import IController, register, register_service, \
     Rule, href
 from inyoka.core.templating import templated, render_template
 from inyoka.core.middlewares import IMiddleware
-from inyoka.core.exceptions import NotFound, Forbidden
+from inyoka.core.exceptions import *
 from inyoka.utils.logger import logger
 from inyoka.i18n import *

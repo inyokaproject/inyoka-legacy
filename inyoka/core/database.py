e23a0e7742f50b6636f63cdd99af5890431a2953
Christopher Grebs <cg@webshox.org>
2009-11-14 18:21:59 +0100

    break python 2.5!!!

--- inyoka/core/database.py
+++ inyoka/core/database.py
@@ -70,7 +70,6 @@
     :copyright: 2009 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-from __future__ import with_statement
 import sys
 from types import ModuleType
 from threading import Lock

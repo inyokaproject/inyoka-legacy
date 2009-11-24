26b0817c38b35166952ad5644d2ce760cf590740
Christopher Grebs <cg@webshox.org>
2009-11-24 17:08:12 +0100

    added test to test that we are using quote=True per default on inyoka.utils.html.escape

---
+++ tests/utils/test_html.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    test_html
+    ~~~~~~~~~
+
+    tests for the inyoka html utilities
+
+    :copyright: 2009 by the Inyok Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from nose.tools import *
+from inyoka.utils.html import escape
+
+
+def test_quoted_escape():
+    eq_(escape(u'My father said: "You rocks!"'),
+        u'My father said: &quot;You rocks!&quot;')
+    eq_(escape(u'My mother "sleeps"', quote=False),
+        u'My mother "sleeps"')

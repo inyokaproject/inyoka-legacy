ee75a2d8b655ccb9dbced07fd184df906cee52d7
Christopher Grebs <cg@webshox.org>
2009-12-04 09:31:33 +0100

    add unittests for inyoka.utils.datastructures.BidiMap

---
+++ tests/utils/test_datastructures.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    test_datastructures
+
+    Unittests for inyoka used datastructures.
+
+    :copyright: 2009 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from inyoka.core.test import *
+from inyoka.utils.datastructures import BidiMap
+
+
+def test_bidimap():
+    map = BidiMap({
+        1: 'dump',
+        2: 'smartly',
+        3: 'clever'
+    })
+    eq_(map[1], 'dump')
+    eq_(map[2], 'smartly')
+    eq_(map[3], 'clever')
+    # and reversed
+    eq_(map['dump'], 1)
+    eq_(map['smartly'], 2)
+    eq_(map['clever'], 3)
+
+
+@raises(ValueError)
+def test_bidimap_unique():
+    map = BidiMap({
+        1: 'maxorious',
+        2: 'enteorious',
+        3: 'barorious',
+        4: 'barorious'
+    })

4f51f95453520138dff22112c090413bb4e09269
Christopher Grebs <cg@webshox.org>
2009-12-17 18:43:00 +0100

    fixed maix' damn unittest what ever... bah!
    ... oh and removed port from base_domain_port :-)

--- inyoka/testing/components.py
+++ inyoka/testing/components.py
@@ -22,39 +22,3 @@ class TestingController(IController):
 
     url_rules = [
     ]
-
-
-#XXX: that's just plain ugly, we need to think of something better here...
-if config['debug']:
-    sys.path.insert(0, os.getcwd())
-    from tests.core.test_subscriptions import Category, Entry, Comment
-
-    class NotifyTrackerMixin(object):
-        tracker = []
-        @classmethod
-        def notify(cls, s, object, subject=None):
-            NotifyTrackerMixin.tracker. \
-                append((cls.name, s.user, object, subject))
-
-
-    class CategorySubscriptionType(SubscriptionType, NotifyTrackerMixin):
-        name = '__test_category'
-        subject_type = Category
-        object_type = Entry
-        mode = 'multiple'
-
-        get_subject = attrgetter('category')
-
-    class BlogSubscriptionType(SubscriptionType, NotifyTrackerMixin):
-        name = '__test_blog'
-        is_singleton = True
-        object_type = Entry
-        mode = 'multiple'
-
-    class CommentsSubscriptionType(SubscriptionType, NotifyTrackerMixin):
-        name = '__test_comments'
-        subject_type = Entry
-        object_type = Comment
-        mode = 'sequent'
-
-        get_subject = attrgetter('entry')

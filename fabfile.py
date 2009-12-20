b4b8c6d97131610e6b85b5a49538c6f09ccbe301
apollo13 <apollo13@apolloner.eu>
2009-12-20 15:25:02 +0100

    call clean_files before starting the tests, can we remove that [localhost] output of clean_files somehow?

--- fabfile.py
+++ fabfile.py
@@ -147,7 +147,7 @@ def test(clean='yes'):
 
     if clean:
         try:
-            local("find . -name '*.pyc' -delete")
+            clean_files()
             os.unlink('.coverage')
             os.unlink('.noseids')
         except:

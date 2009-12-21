6ad616bfe9f7dc941633e2f94c6248a4a07dd28d
Stefan J. Betz <info@stefan-betz.net>
2009-12-21 22:20:56 +0100

    Added imaging.py (inital) to repository

---
+++ inyoka/utils/imaging.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    inyoka.utils.imaging
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Imaging realted stuff (creating thumbnails,resize images,....).
+
+    :copyright: 2009 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""

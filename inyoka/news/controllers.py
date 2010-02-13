9cc76f8afc05156964284317956b34341c429476
Christopher Grebs <cg@webshox.org>
2010-02-13 22:07:15 +0100

    ported template context modifier from -prod

--- inyoka/news/controllers.py
+++ inyoka/news/controllers.py
@@ -8,10 +8,40 @@
     :copyright: 2009 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
+from inyoka.l10n import get_month_names
 from inyoka.core.api import IController, Rule, cache, view
 from inyoka.core.http import Response
 from inyoka.news.models import Article, Category
 
+
+def context_modifier(request, context):
+    """This function adds two things to the context of all pages:
+    `archive`
+        A list of the latest months with articles.
+    `categories`
+        A list of all categories.
+    """
+    key = 'news/archive'
+    data = cache.get(key)
+    if data is None:
+        archive = Article.query.dates('pub_date', 'month')
+        if len(archive) > 5:
+            archive = archive[:5]
+            short_archive = True
+        else:
+            short_archive = False
+        data = {
+            'archive':       archive,
+            'short_archive': short_archive
+        }
+        cache.set(key, data)
+
+    categories = Category.query.cached('news/categories')
+    context.update(
+        months=get_month_names()),
+        categories=categories,
+        **data
+    )
 
 
 class NewsController(IController):
@@ -19,7 +49,13 @@ class NewsController(IController):
 
     url_rules = [
         Rule('/', endpoint='index'),
+#        Rule('/<int:page>/', endpoint='index'),
+#        Rule('/<date:date>/', endpoint='index'),
+#        Rule('/<date:date>/<int:page>/', endpoint='index'),
+#        Rule('/category/<slug>/', endpoint='index'),
+#        Rule('/category/<slug>/<int:page>/', endpoint='index'),
         Rule('/<date:date>/<slug>/', endpoint='detail'),
+#        Rule('/archive/', endpoint='archive'),
     ]
 
     @view('index')
@@ -27,5 +63,5 @@ class NewsController(IController):
         return Response('this is the news (aka ikhaya) index page')
 
     @view('detail')
-    def entry(self, request, date, slug):
+    def detail(self, request, date, slug):
         return Response('this is news entry %r from %s' % (slug, date.strftime('%F')))

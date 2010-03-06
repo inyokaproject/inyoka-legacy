91ff06b06e2ee9bac4471d31b603495e21b980f7
Christopher Grebs <cg@webshox.org>
2010-03-06 10:42:52 +0100

    make tags at maximum 20 characters long

--- inyoka/core/models.py
+++ inyoka/core/models.py
@@ -35,8 +35,8 @@ class TaggedIdentity(db.Model, SerializableObject):
 
     id = db.Column(db.Integer, primary_key=True)
     discriminator = db.Column('type', db.String(50))
-    name = db.Column(db.String(100), nullable=False, index=True, unique=True)
-    slug = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(20), nullable=False, index=True, unique=True)
+    slug = db.Column(db.String(20), nullable=False, unique=True)
 
     __mapper_args__ = {'extension': db.SlugGenerator('slug', 'name'),
                        'polymorphic_on': discriminator}

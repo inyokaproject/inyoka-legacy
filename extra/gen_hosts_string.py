d80775f662b81bb7000349343e1a6c4dd06e4e0d
Christopher Grebs <cg@webshox.org>
2010-04-03 15:33:53 +0200

    added small script that generates the domain map of all subdomains, suitable to add to a /etc/hosts file

---
+++ extra/gen_hosts_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    gen_hosts_string
+    ~~~~~~~~~~~~~~~~
+
+    This script generates the string that must be added to a /etc/hosts file
+    to finally setup inyoka development.
+
+    :copyright: 2010 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from urllib2 import splitport
+from inyoka.core.api import ctx
+
+print u' '.join((sub +'.' if sub else sub) + splitport(ctx.dispatcher.url_adapter.server_name)[0] for sub in sorted(set(rule.subdomain for rule in ctx.dispatcher.url_map.iter_rules())))

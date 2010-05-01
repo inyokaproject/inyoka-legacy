59e9df6e8291389742f689ca9feba33eb0b0eff7
Christopher Grebs <cg@webshox.org>
2010-05-01 15:16:11 +0200

    added a custom babel installer to install the latest stable babel version with the respective cldr version

--- extra/make-bootstrap.py
+++ extra/make-bootstrap.py
@@ -16,8 +16,10 @@ from virtualenv import create_bootstrap_script
 EXTRA_TEXT = """
 import os
 import tempfile, shutil
+from os import path
 
 pil_version = '1.1.7'
+cldr_version = '1.6.1'
 
 
 def pil_install(home_dir):
@@ -26,22 +28,46 @@ def pil_install(home_dir):
     call_subprocess(['wget', 'http://effbot.org/downloads/Imaging-%s.tar.gz' % pil_version], cwd=folder)
     call_subprocess(['tar', '-xzf', 'Imaging-%s.tar.gz' % pil_version], cwd=folder)
 
-    img_folder = os.path.join(folder, 'Imaging-%s' % pil_version)
+    img_folder = path.join(folder, 'Imaging-%s' % pil_version)
 
-    f1 = os.path.join(img_folder, 'setup_new.py')
-    f2 = os.path.join(img_folder, 'setup.py')
+    f1 = path.join(img_folder, 'setup_new.py')
+    f2 = path.join(img_folder, 'setup.py')
 
     file(f1, 'w').write(file(f2).read().replace('import _tkinter', 'raise ImportError()'))
 
-    cmd = [os.path.join(home_dir, 'bin', 'python')]
-    cmd.extend([os.path.join(os.getcwd(), f1), 'install'])
+    cmd = [path.join(home_dir, 'bin', 'python')]
+    cmd.extend([path.join(os.getcwd(), f1), 'install'])
     call_subprocess(cmd)
 
     shutil.rmtree(folder)
 
 
+def babel_svn_repo_install(home_dir):
+    folder = tempfile.mkdtemp(prefix='virtualenv')
+    pypath = path.join(home_dir, 'bin', 'python')
+
+    # checkout cldr
+    call_subprocess(['wget', 'http://unicode.org/Public/cldr/%s/core.zip' % cldr_version], cwd=folder)
+    call_subprocess(['unzip', '-d', 'cldr', 'core.zip'], cwd=folder)
+    cldr_folder = path.join(folder, 'cldr')
+
+    # checkout babel and import/compile respective cldr
+    call_subprocess(['svn', 'co', 'http://svn.edgewall.org/repos/babel/branches/stable/0.9.x/', 'babel'], cwd=folder)
+    babel_folder = path.join(folder, 'babel')
+    cmd = [pypath]
+    cmd.extend([path.join(os.getcwd(), babel_folder, 'scripts/import_cldr.py'),
+                cldr_folder])
+    call_subprocess(cmd)
+
+    cmd = [path.abspath(path.join(home_dir, 'bin', 'python'))]
+    cmd.extend([path.join(os.getcwd(), path.join(babel_folder, 'setup.py')), 'install'])
+    call_subprocess(cmd, cwd=babel_folder)
+    shutil.rmtree(folder)
+
+
 def after_install(options, home_dir):
     easy_install('setuptools', home_dir)
+    babel_svn_repo_install(home_dir)
     easy_install('Jinja2', home_dir)
     easy_install('Werkzeug', home_dir)
     easy_install('Pygments', home_dir)
@@ -50,7 +76,6 @@ def after_install(options, home_dir):
     easy_install('nose', home_dir)
     easy_install('Sphinx', home_dir)
     easy_install('html5lib', home_dir)
-    easy_install('Babel', home_dir)
     easy_install('coverage', home_dir)
     easy_install('minimock', home_dir)
     easy_install('Fabric', home_dir)
@@ -63,7 +88,7 @@ def after_install(options, home_dir):
 
 def easy_install(package, home_dir, optional_args=None):
     optional_args = optional_args or []
-    cmd = [os.path.join(home_dir, 'bin', 'easy_install')]
+    cmd = [path.join(home_dir, 'bin', 'easy_install')]
     cmd.extend(optional_args)
     # update the environment
     cmd.append('-U')

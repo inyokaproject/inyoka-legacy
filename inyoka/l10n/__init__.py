b8363e99d0095174aeba8f0d65d4a1ab6addcbb5
Christopher Grebs <cg@webshox.org>
2010-05-21 14:28:17 +0200

    updated doctests to match babel 1.0 behaviour

--- inyoka/l10n/__init__.py
+++ inyoka/l10n/__init__.py
@@ -182,9 +182,9 @@ def timedeltaformat(datetime_or_timedelta, threshold=.85, granularity='second'):
     Usage Example::
 
         >>> timedeltaformat(timedelta(weeks=12))
-        u'three months ago'
+        u'three mths ago'
         >>> timedeltaformat(timedelta(seconds=30))
-        u'30 seconds ago'
+        u'30 secs ago'
         >>> timedeltaformat(timedelta(seconds=0))
         u'just now'
 
@@ -201,7 +201,7 @@ def timedeltaformat(datetime_or_timedelta, threshold=.85, granularity='second'):
         >>> timedeltaformat(timedelta(hours=23), threshold=0.9)
         u'one day ago'
         >>> timedeltaformat(timedelta(hours=23), threshold=1.1)
-        u'23 hours ago'
+        u'23 hrs ago'
 
     :param datetime_or_timedelta: Either a datetime or timedelta object.
                                   If it's a datetime object we caclculate the

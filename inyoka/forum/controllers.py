54fe9bb7bfed39b0e0510a100fbd75136a27302e
Christopher Grebs <cg@webshox.org>
2010-05-22 12:37:20 +0200

    big performance improvement on forum question detail view

--- inyoka/forum/controllers.py
+++ inyoka/forum/controllers.py
@@ -106,7 +106,7 @@ class ForumController(IController):
     @view('question')
     @templated('forum/question.html', modifier=context_modifier)
     def question(self, request, slug, sort='votes', page=1):
-        question = Question.query.options(db.joinedload('author')) \
+        question = Question.query.options(db.joinedload('votes')) \
                                  .filter_by(slug=slug).one()
         answer_query = Answer.query.filter_by(question=question)
         pagination = URLPagination(answer_query, page)

1c3be092db6d91d50d9eda516aa3649a3791ed7a
Benjamin Wiegand <highwaychile@webshox.org>
2010-06-26 21:44:49 +0200

    added storage system

---
+++ inyoka/core/storage.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+    inyoka.core.storage
+    ~~~~~~~~~~~~~~~~~~~
+
+    This module provides a simple key-based storage with database backend whose
+    data is cached for increased performance. Accessing it is quite simple::
+
+        >>> from inyoka.core.storage import storage
+        >>> storage.set('foo', 'bar')
+        >>> storage.get('foo')
+        'bar'
+        >>> storage.set('bar', 'foo')
+        >>> storage.get_many(['foo', 'bar'])
+        {'foo': 'bar', 'bar': 'foo'}
+
+    :copyright: 2007-2010 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from sqlalchemy.exceptions import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+from inyoka.core.cache import cache
+from inyoka.core.database import db
+
+
+class Storage(db.Model):
+    __tablename__ = 'core_storage'
+
+    key = db.Column(db.String(200), primary_key=True, index=True)
+    value = db.Column(db.PickleType)
+
+
+class storageSchemaController(db.ISchemaController):
+    models = [Storage]
+
+
+class CachedStorage(object):
+    """
+    This is a dict like interface for the `Storage` model.
+    It's used to store cached values also in the database.
+    """
+
+    def get(self, key, default=None, timeout=None):
+        """
+        Get a value from the storage.
+
+        :param key: The key of the requested storage value
+        :param default: The value that's returned if the requested key doesn't
+                        exist. Defaults to None.
+        :param timeout: Give up cache writing after a specific time
+        """
+        value = cache.get('storage/' + key)
+        if value is not None:
+            return value
+
+        result = db.session.execute(
+            db.select([Storage.value]).where(Storage.key == key)
+        ).fetchone()
+
+        if result is None:
+            return default
+
+        value = result[0]
+        self._update_cache(key, value, timeout)
+        return value
+
+    def set(self, key, value, timeout=None):
+        """
+        Set a storage value.
+
+        :param key: The key that should be changed
+        :param value: The new value for the key
+        :param timeout: Give up cache writing after a specific time
+        """
+        try:
+            Storage.query.get(key)
+        except NoResultFound:
+            try:
+                Storage(key=key, value=value)
+                db.session.commit()
+            except IntegrityError:
+                # ignore concurrent insertion
+                return
+        else:
+            Storage.query.filter_by(key=key).update({'value': value})
+            db.session.commit()
+
+        self._update_cache(key, value, timeout)
+
+    def get_many(self, keys, timeout=None):
+        """
+        Get many cached values with just one cache hit or database query.
+
+        :param keys: A list of the requested keys
+        :param timeout: Give up cache writing after a specific time
+        """
+        objects = cache.get_dict(*('storage/%s' % key for key in keys))
+        values = {}
+        for key, value in objects.iteritems():
+            values[key[8:]] = value
+        #: a list of keys that aren't yet in the cache.
+        #: They are queried using a database call.
+        to_fetch = [k for k in keys if values.get(k) is None]
+        if to_fetch:
+            # get the items that are not in cache using a database query
+            query = db.select([Storage.key, Storage.value]) \
+                .where(Storage.key.in_(to_fetch))
+    
+            for key, value in db.session.execute(query):
+                values[key] = value
+                self._update_cache(key, value, timeout)
+        return values
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def _update_cache(self, key, value, timeout=None):
+        cache.set('storage/%s' % key, value, timeout)
+
+
+storage = CachedStorage()
---
+++ tests/core/test_storage.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.core.test_subscriptions
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests for the subscription system.
+
+    :copyright: 2009-2010 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+# XXX ENTFERNEN
+import inyoka
+from inyoka.core.api import db, ctx
+from inyoka.core.test import *
+from inyoka.core.cache import cache
+from inyoka.core.storage import storage
+
+ITEMS1 = [u'foobar', 1, 1.5, ['asd', u'ümlauts'], {'foo': 'bar'}]
+ITEMS2 = [u'barfoo', 2, 5.1, [u'ümlauts', 'asd'], {'bar': 'foo'}]
+DICT_1 = dict((u'key_%d' % i, v) for i, v in enumerate(ITEMS1))
+DICT_2 = dict((u'key_%d' % i, v) for i, v in enumerate(ITEMS2))
+
+
+def test():
+    def test_get(d):
+        for k, v in d.iteritems():
+            cache_key = 'storage/%s' % k
+            # check whether it was also written to the cache
+            eq_(cache.get(cache_key), v)
+            # check whether retrieving storage data of the cache works
+            eq_(storage.get(k), v)
+            # check whether retrieving storage data without cache works
+            cache.delete(cache_key)
+            eq_(storage.get(k), v)
+            # check whether storage.get wrote the data to the cache
+            eq_(cache.get(cache_key), v)
+
+    def test_get_many(d):
+        for k, v in storage.get_many(d.keys()).iteritems():
+            eq_(v, d[k])
+            # check whether storage.get_many wrote the data to the cache
+            eq_(cache.get('storage/%s' % k), v)
+
+    # check whether storage.set works well for new keys
+    for k, v in DICT_1.iteritems():
+        eq_(cache.get('storage/%s' % k), None)
+        storage.set(k, v)
+
+    test_get(DICT_1)
+    cache.delete_many(*DICT_1.keys())
+
+    # check whether storage.set works well for already existing keys
+    for k, v in DICT_2.iteritems():
+        storage.set(k, v)
+
+    test_get(DICT_2)
+
+    # check whether get_many works well if the data is still in the cache
+    test_get_many(DICT_2)
+
+    # check whether get_many works well if the data isn't in the cache anymore
+    cache.delete_many(*DICT_2.keys())
+    test_get_many(DICT_2)

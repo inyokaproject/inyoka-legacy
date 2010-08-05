3df4f72676ed4a2d3a7e4cfbd25656205585bebb
Benjamin Wiegand <highwaychile@webshox.org>
2010-08-05 18:32:01 +0200

    fixed possible circular import that occured when setting deactivated_components

--- inyoka/core/config/__init__.py
+++ inyoka/core/config/__init__.py
@@ -12,6 +12,7 @@
 import os
 from os import path
 from threading import Lock
+from wtforms.validators import ValidationError
 
 
 class ConfigField(object):
@@ -151,7 +152,6 @@ def quote_value(value):
 
 def from_string(value, field):
     """Try to convert a value from string or fall back to the default."""
-    from inyoka.core.forms.validators import ValidationError
     try:
         return field(value)
     except ValidationError:

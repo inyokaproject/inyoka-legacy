a0fc9ca340907fe7b1413a13be25629074cfb8a3
Christopher Grebs <cg@webshox.org>
2010-09-14 21:05:24 +0200

    added CELERY_LOADER definition again, fixes a warning about wrong configuration on loading

--- inyoka/__init__.py
+++ inyoka/__init__.py
@@ -312,6 +312,7 @@ def _bootstrap():
                 realpath(join(dirname(__file__))))
     # the path to the Inyoka instance folder
     os.environ['INYOKA_INSTANCE'] = realpath(join(conts, pardir))
+    os.environ['CELERY_LOADER'] = 'inyoka.core.celery_support.CeleryLoader'
 
     #: bind the context
     ctx = ApplicationContext()

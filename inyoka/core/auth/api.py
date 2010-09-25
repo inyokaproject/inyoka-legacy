eb68c27c7d1b83829e6c2546b64a3b91ed7ca3b5
Christopher Grebs <cg@webshox.org>
2010-09-25 22:52:19 +0200

    added missing core.auth.api module

---
+++ inyoka/core/auth/api.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    inyoka.core.auth.api
+    ~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: 2010 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+from inyoka.core.resource import IResourceManager
+from inyoka.core.config import TextConfigField
+from inyoka.core.auth.models import User, UserProfile, Group, group_group, \
+    user_group
+
+
+class IAuthResourceManager(IResourceManager):
+    """Register core models globally."""
+
+    #: The name of the anonymous user
+    anonymous_name = TextConfigField('anonymous_name', default=u'anonymous')
+
+    #: Set the authsystem to be used.
+    auth_system = TextConfigField('auth.system', default=u'inyoka.portal.auth.EasyAuth')
+
+    models = [User, UserProfile, Group, group_group, user_group]

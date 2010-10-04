f78cfd9e3402812b4e66bd0e388e67005fcc2f79
DasIch <dasdasich@gmail.com>
2010-10-04 22:07:12 +0200

    Removed unnecessary imports

--- inyoka/utils/logger.py
+++ inyoka/utils/logger.py
@@ -8,9 +8,6 @@
     :copyright: 2007-2009 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-import sys
-from inyoka import INYOKA_REVISION
-
 from logbook import Logger
 from logbook.more import ColorizedStderrHandler as ColorizedStderrHandlerBase
 

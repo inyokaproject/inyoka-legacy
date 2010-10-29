acfd1cd49c17b5570062ad160843c463a9bb113b
Benjamin Wiegand <highwaychile@webshox.org>
2010-10-29 21:08:59 +0200

    fixed encoding error in parser

--- inyoka/core/markup/machine.py
+++ inyoka/core/markup/machine.py
@@ -165,7 +165,7 @@ class Renderer(object):
             if obj[0] == '!':
                 pos = obj.index('\0')
                 self.format = obj[1:pos]
-                self.instructions = [soft_unicode(obj[pos + 1:])]
+                self.instructions = [obj[pos + 1:].decode('utf8')]
             elif obj[0] == '@':
                 self.format, self.instructions = loads(obj[1:])
         else:

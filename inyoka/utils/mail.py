f79195fce862137d6ce3b14b7b032c95b37e359e
Christopher Grebs <cg@webshox.org>
2010-11-03 13:35:58 +0100

    removed dnspython dependency, it's unused for now

--- inyoka/utils/mail.py
+++ inyoka/utils/mail.py
@@ -11,8 +11,6 @@
 from email.mime.text import MIMEText
 from email.header import Header
 from subprocess import Popen, PIPE
-from dns.resolver import query as dns_query
-from dns.exception import DNSException
 from inyoka.core.forms.validators import _mail_re
 
 
@@ -34,23 +32,6 @@ def send_mail(subject, message_, from_, to):
 def may_be_valid_mail(email):
     """
     Check if the mail may be valid.  This does not check the hostname part
-    of the email, for that you still have to test with `may_accept_mails`.
+    of the email.
     """
     return _mail_re.match(email) is not None
-
-
-def may_accept_mails(email_or_host, timeout=2):
-    """
-    This function performs a DNS query on the host in the mail or the host
-    provided to see if the server may accept mails.  We don't try to contact
-    the SMTP server listening because most likely a server admin may flag
-    this as suspicious behavior and blacklist us.
-    """
-    host = email_or_host.rsplit('@', 1)[-1]
-    try:
-        answer = list(dns_query(host, 'MX'))
-        if not answer:
-            raise DNSException()
-    except DNSException:
-        return False
-    return bool(answer)

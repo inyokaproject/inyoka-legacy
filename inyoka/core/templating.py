01085cef70ac693ec18abaf3da232e0a4f201083
Sören Wegener <wegener92@googlemail.com>
2010-12-23 21:46:57 +0100

    Typofix.

--- inyoka/core/templating.py
+++ inyoka/core/templating.py
@@ -49,7 +49,7 @@ templates_packages_news = TextConfigField('templates.packages.news', default=u'i
 templates_packages_forum = TextConfigField('templates.packages.forum', default=u'inyoka.forum')
 templates_packages_wiki = TextConfigField('templates.packages.wiki', default=u'inyoka.wiki')
 templates_packages_paste = TextConfigField('templates.packages.paste', default=u'inyoka.paste')
-templates_packages_event = TextConfigField('templates.packages.event', default=u'inyoka.event')
+templates_packages_event = TextConfigField('templates.packages.admin', default=u'inyoka.admin')
 templates_packages_planet = TextConfigField('templates.packages.planet', default=u'inyoka.planet')
 templates_packages_event = TextConfigField('templates.packages.event', default=u'inyoka.event')
 
